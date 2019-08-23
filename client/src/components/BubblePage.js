22f7756f8f3a52e5649d0ce9ee4903f93d0ab930
Neha Patel <neha.r.p@gmail.com>
2019-08-23 09:48:59 -0700

    Part 1 complete, started part 2

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -5,10 +5,7 @@ import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([{
-    color: '',
-    code: {hex: ''}
-  }]);
+  const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
