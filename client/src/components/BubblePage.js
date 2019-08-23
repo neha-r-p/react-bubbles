87e4f338dd4fb21399746fda525f093c864aac5e
Neha Patel <neha.r.p@gmail.com>
2019-08-23 10:30:40 -0700

    PUT request complete...colors can successfully be updated

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -4,7 +4,8 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
-const BubblePage = () => {
+const BubblePage = props => {
+  console.log("bubblepage", props)
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
@@ -23,7 +24,7 @@ const BubblePage = () => {
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList {...props} colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </>
   );
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,7 +7,8 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  console.log("colors", colors);
+  // console.log("props", props)
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,9 +19,25 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log("putty", res);
+      setColorToEdit(initialColor);
+      let colorArr = colors.map(color => {
+        if (colorToEdit.id === color.id){
+          console.log('put')
+          return res.data
+        } else {
+          return color;
+        }
+      });
+      updateColors(colorArr)
+
+    })
+    .catch(err => {
+      console.log(err)
+    })
   };
 
   const deleteColor = color => {
