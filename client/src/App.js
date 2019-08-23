ad0f8916b7fc415bbdebfec480da7234181fb94b
Neha Patel <neha.r.p@gmail.com>
2019-08-23 09:27:22 -0700

    logout working. cannot access bubble page without log in

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Login from "./components/Login";
-import PrivateRoute from "./components/PrivateRoute"
-import BubblePage from "./components/BubblePage"
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 
 import "./styles.scss";
-
 
 function App() {
   const [colorList, setColorList] = useState([]);
   return (
     <Router>
       <div className="App">
+        <div className="links">
+          <Link to="/">Login</Link>
+          <Link to="/bubblepage">Bubble Page</Link>
+          <Link to="/" onClick={() => localStorage.clear()}>
+            Logout
+          </Link>
+        </div>
+
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
